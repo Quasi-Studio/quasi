79ba5975a888585c1628a5bbb2f69e1a0cce1ca8
_Kerman <KermanX@qq.com>
2023-10-14 23:17:50 +0800

    feat: add `vfCreator` component

---
+++ packages/visual-flow/src/view/creator.r.ts
@@ -0,0 +1,34 @@
+import { Content, D, TriggerComponent, TriggerComponentContext, triggerComponent } from "refina";
+import { Block, Graph } from "../model";
+
+const DRAGGING_START_PADDING = 20;
+
+@triggerComponent("vfCreator")
+export class VfCreator extends TriggerComponent<void> {
+  main(_: TriggerComponentContext<void, this>, graph: Graph, inner: D<Content>, factory: () => Block): void {
+    _._div(
+      {
+        onmousedown: (ev) => {
+          ev.preventDefault();
+          const block = factory();
+
+          const padding = graph.boardScale * DRAGGING_START_PADDING;
+          block.nonAttachedPageX = ev.pageX - padding;
+          block.nonAttachedPageY = ev.pageY - padding;
+
+          graph.addBlock(block);
+
+          _.$fire();
+          _.$update();
+        },
+      },
+      inner,
+    );
+  }
+}
+
+declare module "refina" {
+  interface TriggerComponents {
+    vfCreator: VfCreator;
+  }
+}
