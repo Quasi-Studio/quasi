e7351c57bb57a60cb1e8cca96a88f28b644fde4f
_Kerman <KermanX@qq.com>
2023-10-15 10:15:53 +0800

    feat: auto adjusting arrow size when scaling

--- packages/visual-flow/src/components/line/BasicLine.ts
+++ packages/visual-flow/src/components/line/BasicLine.ts
@@ -1,12 +1,12 @@
-import { Line, Socket } from "../../model";
+import { Line } from "../../model";
 import { Point, rotate } from "../../types";
 
 const CTRL_POINT_OFFSET_SCALE = 0.8;
 const CTRL_POINT_OFFSET_MIN = 30;
 
-const ARROW_LENGTH = 25;
-const ARROW_WIDTH = 7;
-const LINE_OFFSET_FOR_ARROW = 25;
+const ARROW_SCALE_TO_BOARD = 3;
+const ARROW_GRAPH_LENGTH_MAX = 25;
+const ARROW_GRAPH_WIDTH_MAX = 7;
 
 function getCtrlPointOffset(delta: number) {
   return Math.max(
@@ -20,7 +20,11 @@ export class BasicLine extends Line {
     let point1 = this.graphPosA;
     let point2 = this.graphPosB;
 
-    point2 = Point.moveFarther(point2, this.b.direction, LINE_OFFSET_FOR_ARROW);
+    const lineOffset =
+      Math.min(1, this.graph.boardScale * ARROW_SCALE_TO_BOARD) *
+      ARROW_GRAPH_LENGTH_MAX;
+
+    point2 = Point.moveFarther(point2, this.b.direction, lineOffset);
 
     const delta = Point.minus(point2, point1);
 
@@ -36,10 +40,17 @@ export class BasicLine extends Line {
   }
 
   get arrowPath() {
+    const arrowLength =
+      Math.min(1, this.graph.boardScale * ARROW_SCALE_TO_BOARD) *
+      ARROW_GRAPH_LENGTH_MAX;
+    const arrowWidth =
+      Math.min(1, this.graph.boardScale * ARROW_SCALE_TO_BOARD) *
+      ARROW_GRAPH_WIDTH_MAX;
+
     const p0 = this.graphPosB;
-    const p1 = Point.moveFarther(p0, this.b.direction, ARROW_LENGTH);
-    const p2 = Point.moveFarther(p1, rotate(this.b.direction), ARROW_WIDTH);
-    const p3 = Point.moveFarther(p1, rotate(this.b.direction), -ARROW_WIDTH);
+    const p1 = Point.moveFarther(p0, this.b.direction, arrowLength);
+    const p2 = Point.moveFarther(p1, rotate(this.b.direction), arrowWidth);
+    const p3 = Point.moveFarther(p1, rotate(this.b.direction), -arrowWidth);
 
     return `M${p0.x} ${p0.y} L${p2.x} ${p2.y} L${p3.x} ${p3.y} Z`;
   }
