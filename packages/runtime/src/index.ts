65ebe49d90fb11e33e28a3c44b6047294e458881
_Kerman <KermanX@qq.com>
2023-10-31 15:10:55 +0800

    upd: add `qInput`

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./appbar.r";
 export * from "./button.r";
+export * from "./input.r";
 
 import QuasiRuntime from "./plugin";
 export default QuasiRuntime;
---
+++ packages/runtime/src/input.r.ts
@@ -0,0 +1,28 @@
+import { ComponentContext, OutputComponent, fromProp } from "refina";
+import QuasiRuntime from "./plugin";
+import "@refina/mdui";
+
+export interface InputProps {
+  label?: string;
+  disabled?: boolean;
+  onInput?: (newVal: string) => void;
+}
+
+export class InputModel {
+  value: string = "";
+}
+
+@QuasiRuntime.outputComponent("qInput")
+export class QInput extends OutputComponent {
+  main(_: ComponentContext<this>, model: InputModel, props: InputProps): void {
+    if (_.mdInput(fromProp(model, "value"), props.label, props.disabled)) {
+      props.onInput?.(_.$ev);
+    }
+  }
+}
+
+declare module "refina" {
+  interface OutputComponents {
+    qInput: QInput;
+  }
+}
