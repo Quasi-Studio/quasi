35bd42e09d19e3b3fcc4c9ed6b85c8fc429e9ff9
_Kerman <KermanX@qq.com>
2023-11-03 14:13:13 +0800

    upd: add textNode component

---
+++ packages/runtime/src/textNode.r.ts
@@ -0,0 +1,19 @@
+import { ComponentContext, OutputComponent } from "refina";
+import QuasiRuntime from "./plugin";
+
+export interface TextNodeProps {
+  text: string | null;
+}
+
+@QuasiRuntime.outputComponent("textNode")
+export class QTextNode extends OutputComponent {
+  main(_: ComponentContext<this>, props: TextNodeProps): void {
+    _.t(props.text ?? "");
+  }
+}
+
+declare module "refina" {
+  interface OutputComponents {
+    textNode: QTextNode;
+  }
+}
