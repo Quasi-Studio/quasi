7c6295cfa9509a537e24f0ebdd01e03a814718f3
_Kerman <KermanX@qq.com>
2023-11-10 17:52:03 +0800

    upd: allow to output undefined as socket

--- packages/northstar/src/utils/toOutpus.ts
+++ packages/northstar/src/utils/toOutpus.ts
@@ -28,30 +28,42 @@ export function toOutput() {
   };
 }
 
-export function multiInSocketToOutput(socket: MultiInSocket): ConnectTo[] {
-  return socket.allConnectedLines.map((l) => ({
-    blockId: l.a.block.id,
-    socketName: l.a.label,
-  }));
+export function multiInSocketToOutput(
+  socket: MultiInSocket | undefined,
+): ConnectTo[] {
+  return (
+    socket?.allConnectedLines.map((l) => ({
+      blockId: l.a.block.id,
+      socketName: l.a.label,
+    })) ?? []
+  );
 }
 
-export function multiOutSocketToOutput(socket: MultiOutSocket): ConnectTo[] {
-  return socket.allConnectedLines.map((l) => ({
-    blockId: (l.b as Socket).block.id,
-    socketName: (l.b as Socket).label,
-  }));
+export function multiOutSocketToOutput(
+  socket: MultiOutSocket | undefined,
+): ConnectTo[] {
+  return (
+    socket?.allConnectedLines.map((l) => ({
+      blockId: (l.b as Socket).block.id,
+      socketName: (l.b as Socket).label,
+    })) ?? []
+  );
 }
 
-export function singleInSocketToOutput(socket: SingleInSocket): ConnectTo {
+export function singleInSocketToOutput(
+  socket: SingleInSocket | undefined,
+): ConnectTo {
   return {
-    blockId: socket.connectedLine?.a.block.id ?? NaN,
-    socketName: socket.connectedLine?.a.label ?? "",
+    blockId: socket?.connectedLine?.a.block.id ?? NaN,
+    socketName: socket?.connectedLine?.a.label ?? "",
   };
 }
 
-export function singleOutSocketToOutput(socket: SingleOutSocket): ConnectTo {
+export function singleOutSocketToOutput(
+  socket: SingleOutSocket | undefined,
+): ConnectTo {
   return {
-    blockId: (socket.connectedLine?.b as Socket)?.block.id ?? NaN,
-    socketName: (socket.connectedLine?.b as Socket)?.label ?? "",
+    blockId: (socket?.connectedLine?.b as Socket | undefined)?.block.id ?? NaN,
+    socketName: (socket?.connectedLine?.b as Socket | undefined)?.label ?? "",
   };
 }
