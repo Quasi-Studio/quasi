ea9a12fa9f77896eb4c8cb3589496a3930e879dd
_Kerman <KermanX@qq.com>
2023-12-17 15:56:01 +0800

    fix: `_.$updateState` has been replaced by `_.$updateContext`

--- packages/northstar/src/app.r.ts
+++ packages/northstar/src/app.r.ts
@@ -28,7 +28,7 @@ setAutoSaveInterval();
 initMonaco();
 
 app.use(FluentUI).use(Vf).use(Basics).use(Monaco)(_ => {
-  if (_.$updateState) {
+  if (_.$updateContext) {
     _.$root.addCls(`fixed top-0 left-0 right-0 bottom-0`);
   }
 
@@ -86,7 +86,7 @@ app.use(FluentUI).use(Vf).use(Basics).use(Monaco)(_ => {
       _.$ref(graphElRef) &&
         _._div({}, _ => _.vfGraph(currentProject.activeGraph));
 
-      if (_.$updateState)
+      if (_.$updateContext)
         _.$window.addEventListener("keydown", ev => {
           if (ev.ctrlKey) {
             if (ev.key === "z" && currentProject.activeGraph.canUndo) {
